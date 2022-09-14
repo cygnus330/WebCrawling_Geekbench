59d94c4f57ee336212d2fd24cf7b409f1c1c9554
sivcde0405 <gs22117@gs.hs.kr>
2022-09-14 18:03:11 +0900

    Changes

--- main.py
+++ main.py
@@ -10,31 +10,57 @@ import pandas as pd
 import openpyxl
 #from crawling import
 
-print('WebCrawling_Geekbench v1.0')
-print('now loading DB (0%)')
+print('''
+░██╗░░░░░░░██╗███████╗██████╗░░█████╗░██████╗░░█████╗░░██╗░░░░░░░██╗██╗░░░░░██╗███╗░░██╗░██████╗░
+░██║░░██╗░░██║██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║██║░░░░░██║████╗░██║██╔════╝░
+░╚██╗████╗██╔╝█████╗░░██████╦╝██║░░╚═╝██████╔╝███████║░╚██╗████╗██╔╝██║░░░░░██║██╔██╗██║██║░░██╗░
+░░████╔═████║░██╔══╝░░██╔══██╗██║░░██╗██╔══██╗██╔══██║░░████╔═████║░██║░░░░░██║██║╚████║██║░░╚██╗
+░░╚██╔╝░╚██╔╝░███████╗██████╦╝╚█████╔╝██║░░██║██║░░██║░░╚██╔╝░╚██╔╝░███████╗██║██║░╚███║╚██████╔╝
+░░░╚═╝░░░╚═╝░░╚══════╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚══════╝╚═╝╚═╝░░╚══╝░╚═════╝░
+
+░██████╗░███████╗███████╗██╗░░██╗██████╗░███████╗███╗░░██╗░█████╗░██╗░░██╗  ██╗░░░██╗░░███╗░░░░░░█████╗░
+██╔════╝░██╔════╝██╔════╝██║░██╔╝██╔══██╗██╔════╝████╗░██║██╔══██╗██║░░██║  ██║░░░██║░████║░░░░░██╔══██╗
+██║░░██╗░█████╗░░█████╗░░█████═╝░██████╦╝█████╗░░██╔██╗██║██║░░╚═╝███████║  ╚██╗░██╔╝██╔██║░░░░░██║░░██║
+██║░░╚██╗██╔══╝░░██╔══╝░░██╔═██╗░██╔══██╗██╔══╝░░██║╚████║██║░░██╗██╔══██║  ░╚████╔╝░╚═╝██║░░░░░██║░░██║
+╚██████╔╝███████╗███████╗██║░╚██╗██████╦╝███████╗██║░╚███║╚█████╔╝██║░░██║  ░░╚██╔╝░░███████╗██╗╚█████╔╝
+░╚═════╝░╚══════╝╚══════╝╚═╝░░╚═╝╚═════╝░╚══════╝╚═╝░░╚══╝░╚════╝░╚═╝░░╚═╝  ░░░╚═╝░░░╚══════╝╚═╝░╚════╝░
+''')
+print('made by sivcde0405')
+
+print('\n\n')
+time.sleep(0.5)
+print('now loading DB (0%)', end = "")
+time.sleep(0.5)
 
 CPU_RAWlist = readCPUlist()
-print('now loading DB (33%)')
+print('\rnow loading DB (33%)', end = "")
+time.sleep(0.5)
 CPU_list = makeCPUlist()
-print('now loading DB (67%)')
+print('\rnow loading DB (67%)', end = "")
+time.sleep(0.5)
 Score_list = makeScorelist()
-print('now loading DB (100%)')
+print('\rnow loading DB (100%)', end = "")
+time.sleep(0.5)
 
 #print(CPU_list)
 #print('\n----\n')
 #print(Score_list)
-print('\n\nnow start crawling (0%)')
+print('\rnow start crawling')
+time.sleep(0.5)
 
 for i in range(12):#12
     for j in range(1, 5):#(1, 5)
         if (CPU_list[i][j] != None):
             print(f'start crawling \"{CPU_list[i][j]}\"')
+            #print(' ')
             CPU_scoreST = []
             CPU_scoreMT = []
             CPU_score = []
 
             for k in range(1, 21):#(1, 21)
+                print(f'\rnow crawling page {k}', end = "")
                 CPU_score.extend(crawlpage(CPU_list[i][j], k))
+            print('\rlisting data', end = "")
             for k in range(len(CPU_score)):
                 CPU_scoreST.append(CPU_score[k][0])
                 CPU_scoreMT.append(CPU_score[k][1])
@@ -46,7 +72,7 @@ for i in range(12):#12
             print(CPU_scoreMT)
             print(mylist)
             '''
-            print(f'CPU Score is {mylist}')
+            print(f'\rCPU Score is {mylist}')
             #print(f'end crawling \"{CPU_list[i][j]}\"')
             Score_list[i][j] = mylist
 
@@ -56,7 +82,8 @@ Score_list_MT = []
 Score_list_ST.append(['Generation', 'i9', 'i7', 'i5', 'i3'])
 Score_list_MT.append(['Generation', 'i9', 'i7', 'i5', 'i3'])
 for i in range(12):
-    print(f'now writing DB ({i/12}%)')
+    print(f'now writing DB ({100*i/12}%)')
+    time.sleep(0.05)
     mylist_ST = []
     mylist_MT = []
     mylist_ST.append(i+1)
